66d6dfd3d84f4f250d30a919b8f55e8668854ad8
Andres Chaverra <adochapi2000@gmail.com>
2024-03-26 21:54:58 -0500

    Added ccs gradients backgrouds

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Profile from "./components/Header/Profile";
 import "./App.css";
-import Lorem from "./components/Lorem/Lorem";
+import MyProjects from "./components/Projects/MyProjects";
 
 const App = () => {
   const [theme, setTheme] = useState("light");
@@ -15,7 +15,7 @@ const App = () => {
         <Header />
         <Profile />
       </main>
-      <Lorem />
+      <MyProjects />
     </main>
   );
 };
---
+++ src/components/Projects/MyProjects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Projects/MyProjects.css";
+
+function MyProjects() {
+  return (
+    <div className="text">
+      <h1 className="projects">My Projects</h1>
+    </div>
+  );
+}
+
+export default MyProjects;

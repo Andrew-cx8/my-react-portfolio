58db106fadd74be14b896bb0e69972e5557d263d
Andres Chaverra <adochapi2000@gmail.com>
2024-04-08 23:15:06 -0500

    Cards projects styles pulished

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../Cards/Card.css";
+import eye from "../../assets/eye.svg";
+import github from "../../assets/github.svg";
 
 function Card(props) {
   return (
@@ -8,9 +10,18 @@ function Card(props) {
         <img id="pimage" src={props.image} alt={props.alt} srcset="" />
         <h2 id="psub">{props.title}</h2>
         <p id="pp">{props.p}</p>
+
         <div className="buttons">
-          <button className="live">Live</button>
-          <button className="code">Code</button>
+          <a target="_blank" href={props.live}>
+            <button className="live">
+              Live <img src={eye} alt="eye icon" />
+            </button>
+          </a>
+          <a target="_blank" href={props.code}>
+            <button className="code">
+              Code <img src={github} alt="octocat" />{" "}
+            </button>
+          </a>
         </div>
       </div>
     </div>
